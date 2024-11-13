4518a87d108b83ad0534b7cc117e53ea2e48709f
stark-18 <anand79054sh@gmail.com>
2024-11-13 13:03:48 +0530

    2

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 
-const qty = document.getElementById("text").value ;
-console.log(text);
+// const qty = document.getElementById("text").value ;
+// console.log(text);
+// function dispprice(){
+//     let price = 15*qty;
+//     document.getElementById("price").innerHTML = price;
+//     window.alert("added"+qty+" to your cart for rupees "+price);
+// }
 
-let price = 15*qty;
-document.getElementById("price").innerHTML = price
+
+// document.getElementById("btn").addEventListener("click",dispprice);
+
+function calculateTotal() {
+    const pricePerUnit = 15;
+    const quantity = document.getElementById("text").value;
+    const totalPrice = pricePerUnit * quantity;
+
+    document.getElementById("price").textContent = "Total price : Rs:"+totalPrice;
+    window.alert("added ${quantity}to your cart for rupees ${totalprice} ");
+}
+
+document.getElementById("btn").addEventListener("click",calculateTotal);
